fd23ce1e110c1be9c5b5a1529d51dcac9750f519
Michael Ekstrand <md@ekstrandom.net>
2019-07-17 21:36:42 -0600

    add BX

--- lkdemo/datasets.py
+++ lkdemo/datasets.py
@@ -5,3 +5,6 @@ mlsmall = ds.MovieLens('data/ml-latest-small')
 ml100k = ds.ML100K('data/ml-100k')
 ml1m = ds.ML1M('data/ml-1m')
 ml10m = ds.ML10M('data/ml-10M100K')
+
+if hasattr(ds, 'BookCrossing'):
+    bx = ds.BookCrossing('data/bx')
