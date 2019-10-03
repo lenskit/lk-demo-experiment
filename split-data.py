b11a85e3084019f6d68be9115fa19920338bc5c3
NgoziI <ngoiheme@gmail.com>
2019-10-03 14:45:33 -0500

    added the split-data.py module

---
+++ split-data.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Usage:
+    train-model.py [-p partitions] [-d destination] DATASET
+
+Options:  
+    -p partitions     number of cross-folds [default: 5]
+    -d destination    destination directory [default: splitData]
+    DATASET           name of data set to load  
+"""
+
+from docopt import docopt
+from lkdemo import datasets, log
+from pathlib import Path
+import lenskit.crossfold as xf
+
+_log = log.script(__file__)
+
+args = docopt(__doc__)
+
+dsname = args.get('DATASET')
+partitions = int(args.get('-p'))
+dest = args.get('-d')
+
+_log.info('locating data set %s', dsname)
+data = getattr(datasets, dsname)
+
+_log.info('loading ratings')
+ratings = data.ratings
+
+path = Path(dest)
+path.mkdir(exist_ok=True, parents=True)
+
+
+_log.info('writing to %s', path)
+testRowsPerUsers = 5
+for i, tp in enumerate(xf.partition_users(ratings, partitions, xf.SampleN(testRowsPerUsers)),1):
+    tp.train.to_csv(path / f'train-{i}.csv', index=False)
+    tp.test.to_csv(path / f'test-{i}.csv' , index=False)
+    
