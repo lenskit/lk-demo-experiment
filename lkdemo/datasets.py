be842e40574d3e635f88940933bca0d40236d944
Michael Ekstrand <michaelekstrand@boisestate.edu>
2023-04-15 12:32:21 -0600

    ml-25m

--- lkdemo/datasets.py
+++ lkdemo/datasets.py
@@ -6,6 +6,7 @@ import pandas as pd
 from lenskit import datasets as ds
 
 ml20m = ds.MovieLens('data/ml-20m')
+ml25m = ds.MovieLens('data/ml-25m')
 mlsmall = ds.MovieLens('data/ml-latest-small')
 ml100k = ds.ML100K('data/ml-100k')
 ml1m = ds.ML1M('data/ml-1m')
