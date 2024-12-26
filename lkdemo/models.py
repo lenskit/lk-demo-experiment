56e0f165b856114f38fc3708ac7d6d24f5ad4526
Michael Ekstrand <mdekstrand@drexel.edu>
2024-12-26 09:34:18 -0600

    fix update for new lenski

--- lkdemo/models.py
+++ lkdemo/models.py
@@ -3,7 +3,7 @@ This module defines the various models, and their default configurations, that
 we are going to test in our experiment.
 """
 
-from lenskit.als import BiasedMF, ImplicitMF
+from lenskit.als import BiasedMFScorer, ImplicitMFScorer
 from lenskit.basic import BiasScorer, PopScorer
 from lenskit.knn import ItemKNNScorer, UserKNNScorer
 
@@ -11,6 +11,6 @@ Bias = BiasScorer(damping=5)
 Pop = PopScorer()
 IIE = ItemKNNScorer(20, save_nbrs=2500, feedback="explicit")
 UUE = UserKNNScorer(30)
-ALS = BiasedMF(50)
-IALS = ImplicitMF(50)
+ALS = BiasedMFScorer(50)
+IALS = ImplicitMFScorer(50)
 III = ItemKNNScorer(20, save_nbrs=2500, feedback="implicit")
