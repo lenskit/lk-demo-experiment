118f04f455a25a1b9d6a80a8ca032d8da09ad3ff
Michael Ekstrand <mdekstrand@drexel.edu>
2025-03-20 17:16:51 -0400

    rerun ML1M with FlexMF

--- lkdemo/models.py
+++ lkdemo/models.py
@@ -15,6 +15,8 @@ UUE = UserKNNScorer(max_nbrs=30)
 ALS = BiasedMFScorer(embedding_size=50)
 IALS = ImplicitMFScorer(embedding_size=50)
 III = ItemKNNScorer(max_nbrs=20, save_nbrs=2500, feedback="implicit")
-FlexBPR = FlexMFImplicitScorer(embedding_size=50, epochs=20, loss="pairwise")
+FlexBPR = FlexMFImplicitScorer(
+    embedding_size=50, epochs=20, loss="pairwise", item_bias=False
+)
 FlexLMF = FlexMFImplicitScorer(embedding_size=50, epochs=20, loss="logistic")
 FlexMF = FlexMFExplicitScorer(embedding_size=50, epochs=20)
