1a7581e4f2d3bb452f442d21de71360b67131321
Michael Ekstrand <mdekstrand@drexel.edu>
2025-03-20 16:51:44 -0400

    add flex models

--- lkdemo/models.py
+++ lkdemo/models.py
@@ -5,6 +5,7 @@ we are going to test in our experiment.
 
 from lenskit.als import BiasedMFScorer, ImplicitMFScorer
 from lenskit.basic import BiasScorer, PopScorer
+from lenskit.flexmf import FlexMFExplicitScorer, FlexMFImplicitScorer
 from lenskit.knn import ItemKNNScorer, UserKNNScorer
 
 Bias = BiasScorer(damping=5)
@@ -14,3 +15,6 @@ UUE = UserKNNScorer(max_nbrs=30)
 ALS = BiasedMFScorer(embedding_size=50)
 IALS = ImplicitMFScorer(embedding_size=50)
 III = ItemKNNScorer(max_nbrs=20, save_nbrs=2500, feedback="implicit")
+FlexBPR = FlexMFImplicitScorer(embedding_size=50, loss="pairwise")
+FlexLMF = FlexMFImplicitScorer(embedding_size=50, loss="logistic")
+FlexMF = FlexMFExplicitScorer(embedding_size=50)
